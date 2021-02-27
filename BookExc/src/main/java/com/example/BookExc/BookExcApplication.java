9e2c2074f322b01778cec0bcafc2a0c6b2351ebe
ergunberkan <67101608+ergunberkan@users.noreply.github.com>
2021-02-27 13:55:18 +0300

    BookExc project was created.

---
+++ BookExc/src/main/java/com/example/BookExc/BookExcApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookExc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookExcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookExcApplication.class, args);
+	}
+
+}
---
+++ BookExc/src/test/java/com/example/BookExc/BookExcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookExc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookExcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

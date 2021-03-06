b02d671c8d5f1df94494fa3fb0ff7cc4ddf6c061
ergunberkan <67101608+ergunberkan@users.noreply.github.com>
2021-03-06 23:16:18 +0300

    Junit Tests

--- BookExc/src/test/java/com/example/BookExc/BookExcApplicationTests.java
+++ BookExc/src/test/java/com/example/BookExc/BookExcApplicationTests.java
@@ -1,9 +1,8 @@
 package com.example.BookExc;
 
-import com.jayway.jsonpath.JsonPath;
-import org.junit.experimental.results.ResultMatchers;
+
+
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,7 +15,7 @@ import  org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import  org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class BookExcApplicationTests {

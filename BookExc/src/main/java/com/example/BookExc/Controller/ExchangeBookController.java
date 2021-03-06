cc1897a82e705af876663f6377ca10e3cb728376
ergunberkan <67101608+ergunberkan@users.noreply.github.com>
2021-03-06 23:16:49 +0300

    Add Api Operation İnformations

--- BookExc/src/main/java/com/example/BookExc/Controller/ExchangeBookController.java
+++ BookExc/src/main/java/com/example/BookExc/Controller/ExchangeBookController.java
@@ -4,6 +4,7 @@ import com.example.BookExc.domain.ExchangeBookDO;
 import com.example.BookExc.dto.ExchangeBookDTO;
 import com.example.BookExc.service.IUserService;
 import com.example.BookExc.service.InfExcBookService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,31 +20,35 @@ public class ExchangeBookController {
      */
     @Autowired
     private InfExcBookService bookService;
-
+    @ApiOperation("New add Book Exchange")
     @PostMapping(value = "/AddExchange")
     public ResponseEntity<ExchangeBookDTO> addExchange(@RequestBody ExchangeBookDO exchangeBookDO)
     {
         ExchangeBookDO addExc= bookService.createExchange(exchangeBookDO);
         return new ResponseEntity(addExc, HttpStatus.CREATED);
     }
+    @ApiOperation("Update Book Exchange Information")
     @PutMapping(value = "/UpdateExchange",consumes = "application/json",produces = "application/json")
     public ResponseEntity<ExchangeBookDTO> updateExchange(@RequestBody ExchangeBookDO exchangeBookDO)
     {
         ExchangeBookDO updateExc= bookService.updateExchange(exchangeBookDO);
         return new ResponseEntity(updateExc,HttpStatus.CREATED);
     }
+    @ApiOperation("Get Book Exchange Information by Book Exchange Id")
     @GetMapping("/Exchanges/{ExchangeNo}")
     public ResponseEntity<ExchangeBookDTO> getByIdExc(@PathVariable(value = "ExchangeNo") Long ExchangeNo)
     {
         ExchangeBookDO exchangeBookDO= bookService.getExchange(ExchangeNo);
         return new ResponseEntity(exchangeBookDO,HttpStatus.OK);
     }
-    @GetMapping(value = "Exchanges",consumes = "application/json",produces = "application/json")
+    @ApiOperation("Get All Book Exchange Information")
+    @GetMapping(value = "Exchanges")
     public ResponseEntity<List<ExchangeBookDTO>> getAllExchanges()
     {
        List<ExchangeBookDO> AllExchanges= bookService.getAllExchange();
         return new ResponseEntity(AllExchanges,HttpStatus.OK);
     }
+    @ApiOperation("Delete Book Exchange Information by Book Exchanges ID")
     @DeleteMapping("/Exchanges/{ExchangeNo}")
     public ResponseEntity<ExchangeBookDTO> deleteByExchangeNo(@PathVariable(value = "ExchangeNo") Long ExchangeNo)
     {
--- BookExc/src/main/java/com/example/BookExc/Controller/UserController.java
+++ BookExc/src/main/java/com/example/BookExc/Controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.BookExc.domain.UsersDO;
 import com.example.BookExc.dto.UserDTO;
 import com.example.BookExc.service.IUserService;
 import com.example.BookExc.service.InfExcBookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,29 +20,33 @@ public class UserController {
     @Autowired
     private IUserService iUserService;
 
-
+    @ApiOperation("New add user")
     @PostMapping(path = "/addUser", consumes = "application/json",produces = "application/json")
     public ResponseEntity<UserDTO> createUser(@RequestBody UsersDO usersDO){
         UsersDO createdUser= iUserService.createUsers(usersDO);
         return new ResponseEntity(createdUser, HttpStatus.CREATED);
     }
+    @ApiOperation("Updates User")
     @PutMapping(path = "/update", consumes = "application/json",produces = "application/json")
     public ResponseEntity<UsersDO> updateUser(@RequestBody UsersDO usersDO)
     {
         UsersDO updatedUsers=iUserService.updateUsers(usersDO);
         return new ResponseEntity<>(updatedUsers,HttpStatus.CREATED);
     }
+    @ApiOperation("Get User Information by UserId")
     @GetMapping(path = "/users/{userId}")
     public ResponseEntity<UsersDO> getUsers(@PathVariable(value = "userId") Long userId) {
         UsersDO user = iUserService.getUsers(userId);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
+    @ApiOperation( "Get all Users Information")
     @GetMapping(path = "/users")
     public ResponseEntity<List<UsersDO>> getAllUsers()
     {
         List<UsersDO> allUsers=iUserService.getAllUsers();
         return new ResponseEntity<>(allUsers,HttpStatus.OK);
     }
+    @ApiOperation("User delete by User Id")
     @DeleteMapping(path = "/users/{userId}")
     public ResponseEntity<String> deleteUser(@PathVariable(value = "userId") Long userId) {
         iUserService.deleteUsers(userId);
